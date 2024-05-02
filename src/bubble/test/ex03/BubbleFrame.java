2e7882332019e935d0251ef004da2d26d948a399
ddg999 <slowman918@gmail.com>
2024-05-02 15:06:21 +0900

    bubble 3단계완성

---
+++ src/bubble/test/ex03/BubbleFrame.java
@@ -0,0 +1,82 @@
+package bubble.test.ex03;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame {
+
+	private JLabel backgroundMap;
+	private Player player;
+
+	public BubbleFrame() {
+		initData();
+		setInitLayout();
+		addEventListener();
+
+		// Player 백그라운드 서비스 시작
+
+		new Thread(new BackgroundPlayerService(player)).start();
+
+	}
+ 
+	private void initData() {
+		// todo 이미지 변경
+		backgroundMap = new JLabel(new ImageIcon("img/backgroundMapService.png"));
+		// backgroundMap = new JLabel(new ImageIcon("img/test.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(backgroundMap); // Frame -> root Panel에 add 처리
+		setSize(1000, 640);
+
+		player = new Player();
+	}
+
+	private void setInitLayout() {
+		setLayout(null); // 좌표 값으로 배치
+		setResizable(false); // 프레임 조절 불가
+		setLocationRelativeTo(null); // JFrame을 모니터 가운데 자동 배치
+		setVisible(true);
+		add(player);
+	}
+
+	private void addEventListener() {
+		this.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println("key code : " + e.getKeyCode());
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.left();
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.right();
+					break;
+				case KeyEvent.VK_UP:
+					player.up();
+					break;
+				}
+			} // end of KeyPressed
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			} // end of KeyReleased
+
+		});
+	}
+
+	// 코드 테스트
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+}

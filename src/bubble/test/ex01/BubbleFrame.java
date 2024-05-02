a9f3b5967e9e271b0fd564e03f34a82bc8efd82c
ddg999 <slowman918@gmail.com>
2024-05-02 12:09:39 +0900

    bubble 1단계완성

---
+++ src/bubble/test/ex01/BubbleFrame.java
@@ -0,0 +1,67 @@
+package bubble.test.ex01;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame {
+
+	private JLabel backgroundMap;
+	private Player player;
+
+	public BubbleFrame() {
+		initData();
+		setInitLayout();
+		addEventListener();
+	}
+
+	private void initData() {
+		backgroundMap = new JLabel(new ImageIcon("img/backgroundMap.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(backgroundMap); // Frame -> root Panel에 add 처리
+		setSize(1000, 640);
+
+		player = new Player();
+	}
+
+	private void setInitLayout() {
+		setLayout(null); // 좌표 값으로 배치
+		setResizable(false); // 프레임 조절 불가
+		setLocationRelativeTo(null); // JFrame을 모니터 가운데 자동 배치
+		setVisible(true);
+		add(player);
+	}
+
+	private void addEventListener() {
+		this.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println("key code : " + e.getKeyCode());
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					// 구현
+					player.left();
+					break;
+				case KeyEvent.VK_RIGHT:
+					// 구현
+					player.right();
+					break;
+				case KeyEvent.VK_UP:
+					// 구현
+					player.up();
+					break;
+				}
+
+			} // end of KeyPressed
+
+		});
+	}
+
+	// 코드 테스트
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+}
